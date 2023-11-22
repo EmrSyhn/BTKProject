7f9f2b3d3509b2cb6e3efc7d0bf9870cfdc3acd4
Emir <emirseyhana.z@gmail.com>
2023-11-22 16:17:49 +0300

    Initial Commit

---
+++ BTKProject/BTKProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTKProjectApp.swift
+//  BTKProject
+//
+//  Created by Emir Seyhan on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BTKProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
